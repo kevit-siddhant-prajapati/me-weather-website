3413e64b14944a588b483d8fcc59db10b7133759
Siddhant Prajapati <siddhant.prajapati@kevit.io>
2023-09-01 10:52:43 +0530

    change url localhost to render url

--- public/js/src.js
+++ public/js/src.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('https://weatherapp-7vmb.onrender.com:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log('Something went wrong')
