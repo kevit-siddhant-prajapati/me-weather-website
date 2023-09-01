5bedceaa6e98e7f4c069ad693d5ad759c6efb1c5
Siddhant Prajapati <siddhant.prajapati@kevit.io>
2023-09-01 11:12:53 +0530

    change url to /weather?address=

--- public/js/src.js
+++ public/js/src.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch(':3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log('Something went wrong')
